4f9ef61def5b2be1fd9e4049eeaa457a4bd01373
techs-sus <92276908+techs-sus@users.noreply.github.com>
2025-01-22 20:10:07 -0500

    tests: fix path specifier + unify naming

--- scripts/run_tests.ps1
+++ scripts/run_tests.ps1
@@ -1,2 +1,2 @@
-rojo build -o place.rbxl .\place.project.json
-run-in-roblox --place place.rbxl --script .\scripts\run_tests.luau
+rojo build -o place.rbxl ./place.project.json
+run-in-roblox --place place.rbxl --script ./scripts/run_tests.luau
