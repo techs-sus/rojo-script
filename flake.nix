ee83857b888b6ce82b3c6697dad8991cb1e8e655
techs-sus <92276908+techs-sus@users.noreply.github.com>
2025-02-01 11:21:44 -0500

    flake: fix binary cache not working for wally-nix

--- flake.nix
+++ flake.nix
@@ -1,33 +1,54 @@
 {
   description = "a development shell which provides an environment containing rust, bun, and rojo";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
+  # binary cache for wally-nix (needs to be in the root flake too)
+  nixConfig = {
+    extra-substituters = "https://techs-sus-wally-nix.cachix.org";
+    extra-trusted-public-keys = "techs-sus-wally-nix.cachix.org-1:hOye+Fj1heELMgDJOzDoQnFLsQA/kVN0ZVRnZmsAyB4=";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    wally-nix = {
+      url = "github:techs-sus/wally-nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, wally-nix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        wally = wally-nix.packages.${system}.default;
       in
       {
-        devShells.default = with pkgs; mkShell {
+        devShells.default = pkgs.mkShell {
           buildInputs = [
-            (rust-bin.stable.latest.default.override {
+            (pkgs.rust-bin.stable.latest.default.override {
               extensions = [ "rust-analyzer" "rust-src" ];
             })
           ];
 
           packages = [
-            bun
-            rojo
+            pkgs.bun
+            pkgs.rojo
 
-            # TODO: how would run-in-roblox work? wally is pending a custom nix package
+            # TODO: how would run-in-roblox work?
+            wally
           ];
 
 
